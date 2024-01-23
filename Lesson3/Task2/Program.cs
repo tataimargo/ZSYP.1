0888ca238ce95f2db4b2a3a0537a39994aa3ebe0
Tatyana <potap898@mail.ru>
2024-01-23 15:18:29 +0400

    add lesso3:Task2

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintSqueares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+PrintSqueares(5);
+PrintSqueares(10);
+PrintSqueares(15);
